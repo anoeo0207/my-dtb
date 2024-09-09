2e5ef02e5670c922a304f86d5a32abc854e38360
Nguyen Hai An <pejij2004@gmail.com>
2024-09-09 23:59:31 +0700

    Acme Proj

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,8 +1,8 @@
 import DashboardSkeleton from "@/app/ui/skeletons";
 
 export default function Loading() {
-    return <div>Loading...</div>; //SideNav is static => still can interact with SideNav
-    //return <DashboardSkeleton />;
+    //return <div>Loading...</div>; //SideNav is static => still can interact with SideNav
+    return <DashboardSkeleton />;
 }
 
 //User dont have to wait for the page to finish loading (interruptable navigation)
