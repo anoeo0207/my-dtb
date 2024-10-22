927d2b9a2c4a1185eb8ad6f049ef2386dc53b16d
Hai An Nguyen <haiannguyen@Hais-MacBook-Pro.local>
2024-10-22 16:34:25 +0700

    2

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
     experimental: {
         ppr: 'incremental', //incremental value allows to adopt PPR for specific routes
     },
-    rules: {
-        "react/prop-types": "off",
-      },
+    eslint: {
+        ignoreDuringBuilds: true, 
+    },
 };
 
 export default nextConfig;
