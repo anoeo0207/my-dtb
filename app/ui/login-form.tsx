4a703b27bb330b083e0f28fab18667001175f81d
Hai An Nguyen <haiannguyen@Hais-MacBook-Pro.local>
2024-10-23 16:18:03 +0700

    Update

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,72 +1,68 @@
 'use client';
 
-import { lusitana } from '@/app/ui/fonts';
+import React from 'react'; 
 import {
-  AtSymbolIcon,
-  KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+import { Button } from '@/components/ui/button';
+import { Label } from '@/components/ui/label';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent} from '@/components/ui/card';
 import { useActionState } from 'react';
 import { authenticate } from '../lib/action';
+import { UserRound, LockIcon } from 'lucide-react';
+import Input from '@/components/ui/input';
 
 export default function LoginForm() {
-  const [errorMessage, formAction, isPending] = useActionState(
+  const [errorMessage, formAction] = useActionState(
     authenticate,
     undefined,
   );
 
   return (
     <form action={formAction} className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-          Please log in to continue.
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+      <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl text-center">Login</CardTitle>
+        <CardDescription className="text-center">
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2 flex">
+            <div className="flex items-center">
+            <UserRound className="h-6 w-6" />
+            <Label htmlFor="email" className="pl-2">Email </Label>
             </div>
+            <Input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter your email address"
+              required
+            />
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+          <div className="grid gap-2">
+            <div className="flex items-center">
+                <div className="flex items-center">
+                    <LockIcon className="h-6 w-6" />
+                    <Label htmlFor="password" className="pl-2">Password</Label>
+                </div>
             </div>
+            <Input id="password" type="password" name="password" minLength={6} placeholder="Enter your password" required />
           </div>
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
         </div>
-        <Button className="mt-4 w-full" aria-disabled={isPending}>
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+        </div>
+      </CardContent>
+    </Card>
         <div className="flex h-8 items-end space-x-1">
           {errorMessage && (
             <>
@@ -75,7 +71,6 @@ export default function LoginForm() {
             </>
           )}
         </div>
-      </div>
     </form>
   );
 }
